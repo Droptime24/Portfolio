aafa88e715fb0e8edaa0d07c5a49e79acbe51146
David Luce <david.luce@outlook.com>
2019-07-13 23:40:39 -0600

    redid site

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$("document").ready(function () {
+    $(".button-collapse").sideNav();
+    $('.carousel').carousel();
+    $('.parallax').parallax();
+});
+
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+});
+
+
